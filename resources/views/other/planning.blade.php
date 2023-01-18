cef3b0ce7a3568a7eeadc712b918d329806217a6
hamidrezakiani <hamidreza.behrad96@gmail.com>
2023-01-19 03:16:05 +0330

    planning

--- resources/views/other/planning.blade.php
+++ resources/views/other/planning.blade.php
@@ -9,7 +9,7 @@ active
 
 @section('content')
   <div class="row">
-    <form action="{{url('/planning')}}" method="POST">
+    <form action="{{url('/updatePlanning')}}" method="POST">
         @csrf
         <textarea name="text" id="editor1" rows="10" cols="80">
                 {{$value->value}}
