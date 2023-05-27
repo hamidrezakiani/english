509fdb31c1ce5219c768d2ed18943f7fd608087f
hamidrezakiani <hamidreza.behrad96@gmail.com>
2023-05-27 20:09:10 +0330

    add update profile api

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,6 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return 6;
             return route('login');
         }
     }
