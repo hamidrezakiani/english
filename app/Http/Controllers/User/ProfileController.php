beb68c69fe90236a516105e482e1e21be37e8035
hamidrezakiani <hamidreza.behrad96@gmail.com>
2023-05-27 20:24:38 +0330

    add update profile api

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -3,14 +3,40 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Lib\ResponseTemplate;
+use App\Models\User;
 use Auth;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    use ResponseTemplate;
     public function update(Request $request)
     {
         $user = Auth::guard('api')->user();
-        return response()->json(['user' => $user],200);
+        if($request->invitationCode)
+        {
+            $inviter = User::where('invitation_code',$request->invitationCode)->first();
+            if($inviter)
+              $invited_by = $inviter->id;
+            else
+            {
+               $this->status(422);
+               $this->errors([
+                 'invitationCode' => [
+                    0 => 'کد معرف اشتباه است',
+                 ]
+               ]);
+               return $this->response();
+            }
+        }
+        else
+          $invited_by = $user->invited_by;
+
+        $user->update([
+            'name' => $request->name,
+            'invited_by' => $invited_by
+        ]);
+        return $this->response();
     }
 }
