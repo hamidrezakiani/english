0e42bd53bb9a7782bcd965cd8202c8722aaf649f
hamidrezakiani <hamidreza.behrad96@gmail.com>
2023-10-17 00:20:02 +0330

    hgh

--- resources/views/failed-pay.blade.php
+++ resources/views/failed-pay.blade.php
@@ -11,7 +11,7 @@
       style="width: 100%;display: flex;flex-direction: column;justify-content: center;align-items: center;"
     >
          <h1 style="color: red;text-align: center;margin-top: 150px">پرداخت نا موفق!</h1>
-         <a href="exp://192.168.16.137:19000?pay=failed" style="text-decoration: none;background-color: darkmagenta;color: #fff;border-radius: 10px;font-size: 18px;padding: 5px 10px;margin-top: 100px">بازگشت به برنامه</a>
+         <a href="exp://192.168.81.3:19000?pay=failed" style="text-decoration: none;background-color: darkmagenta;color: #fff;border-radius: 10px;font-size: 18px;padding: 5px 10px;margin-top: 100px">بازگشت به برنامه</a>
     </div>
 </body>
 </html>
