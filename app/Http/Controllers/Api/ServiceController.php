993217921356a7ea1b58157ef26e177e198d9276
hamidrezakiani <hamidreza.behrad96@gmail.com>
2024-06-04 05:45:44 +0330

    add amount

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -29,7 +29,7 @@ class ServiceController extends Controller
               'service_id' => $service->id,
               'payable' => $service->amount
            ]);
-         $this->setData(['amount' => $order->payable,"discount" => $order->discount->amount ,'order_id' => $order->id]); 
+         $this->setData(['amount' => $order->payable,"discount" => $order?->discount?->amount ?? 0 ,'order_id' => $order->id]); 
          return $this->response();
     }
     public function setDiscount(Request $request)
