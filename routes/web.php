4f5cd4d60824cd4486a53ddcdba6b7ff0763c954
hamidrezakiani <hamidreza.behrad96@gmail.com>
2024-06-28 05:14:13 +0330

    free trial

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['prefix' => 'panel'],function(){
+   dd(Carbon\Carbon::now());
     Route::get('/',function(){
        return view('dashboard');
     });
