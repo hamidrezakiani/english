73344d2acb79a920711458f7aa69d193971392df
hamidrezakiani <hamidreza.behrad96@gmail.com>
2024-06-28 05:10:55 +0330

    free trial

--- app/Http/Controllers/Api/ReadingTestController.php
+++ app/Http/Controllers/Api/ReadingTestController.php
@@ -14,7 +14,7 @@ class ReadingTestController extends Controller
     public function index(Request $request)
     {
         $this->lastUpdatedAt = $request->lastUpdatedAt;
-        $currentTime = Carbon::now('Asia/Tehran');
+        $currentTime = Carbon::now('UTC');
         if (!$this->lastUpdatedAt){
             \Log::debug($request->lastUpdatedAt);
             $tests = $this->withoutDeleted();
